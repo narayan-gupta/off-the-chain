e11780982b1b4ba8a5396c628fa003153cde5b57
Narayan Gupta <narayangupta@Narayans-MacBook-Pro.local>
2021-12-19 21:07:53 -0700

    Update contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,9 +2,13 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.7.0;
 
+
+// This is the main building block for smart contracts.
 contract Token {
-    string public name = "DumbAssCoin"
-    string public symbol = "DAC"
+    // Some string type variables to identify the token.
+    // The `public` modifier makes a variable readable from outside the contract.
+    string public name = "My Hardhat Token";
+    string public symbol = "MHT";
 
     // The fixed amount of tokens stored in an unsigned integer type variable.
     uint256 public totalSupply = 1000000;
@@ -15,18 +19,25 @@ contract Token {
     // A mapping is a key/value map. Here we store each account balance.
     mapping(address => uint256) balances;
 
-    
     /**
      * Contract initialization.
      *
      * The `constructor` is executed only once when the contract is created.
      */
     constructor() {
+        // The totalSupply is assigned to transaction sender, which is the account
+        // that is deploying the contract.
         balances[msg.sender] = totalSupply;
         owner = msg.sender;
     }
 
-    function transfer(address to, uuint256 amount) {
+    /**
+     * A function to transfer tokens.
+     *
+     * The `external` modifier makes a function *only* callable from outside
+     * the contract.
+     */
+    function transfer(address to, uint256 amount) external {
         // Check if the transaction sender has enough tokens.
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
@@ -37,6 +48,12 @@ contract Token {
         balances[to] += amount;
     }
 
+    /**
+     * Read only function to retrieve the token balance of a given account.
+     *
+     * The `view` modifier indicates that it doesn't modify the contract's
+     * state, which allows us to call it without executing a transaction.
+     */
     function balanceOf(address account) external view returns (uint256) {
         return balances[account];
     }
